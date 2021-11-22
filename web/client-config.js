d1bf451d9424e16b29efbdcc3a48fed2a2f29a13
kathmbeck <katherine.murphy.beck@gmail.com>
2021-11-22 08:35:57 -0800

    testing preview with updated source plugin

--- web/client-config.js
+++ web/client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID || '1pmvpzxo',
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'rtp2vi9e',
     dataset: process.env.GATSBY_SANITY_DATASET || 'production'
   }
 }
